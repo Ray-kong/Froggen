9ab296207d8cc3966a5032728d13c53f7e1a88b8
RayKong <ray.hyun.kong@gmail.com>
2023-03-22 16:29:50 -0400

    Score System
    Added Score System with one example Item

---
+++ Assets/Script/ItemCollector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int score = 0;
+
+    [SerializeField] private Text ScoreText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Item")) {
+            Destroy(collision.gameObject);
+            score++;
+            Debug.Log("score: " + score);
+            ScoreText.text = "Score: " + score;
+        }
+    }
+
+
+} 
