db84be0a6855531e223f63b662fe2a0f1655f0c5
djrosenthal27 <djrosenthal004@gmail.com>
2023-03-27 14:29:45 -0400

    Moving Death Objects added
    I created some scripts that enable three types of death objects: Track (moving back and forth), Falling (falls once on a trigger and stays where it is after hitting the ground), and Loop (moves to a point then respawns).

---
+++ Assets/Script/Anvil.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anvil : MonoBehaviour
+{
+
+    private bool stopped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopped = false;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            transform.gameObject.tag = "Untagged";
+        }
+    }
+}
---
+++ Assets/Script/MovingObject.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+
+    public float endX;
+    public float endY;
+    public float velocityX;
+    public float velocityY;
+
+    // type is one of the following:
+    // Track: object on a track, moves back and forth (such as buzzsaw)
+    // Falling: object that falls once and stays in the scene (such as anvil)
+    // Loop: object that falls, disappears, and falls again along the same path (such as wrench)
+    public string type;
+
+    private float posX;
+    private float posY;
+    private float startX;
+    private float startY;
+    private bool falling;
+
+    void Start()
+    {
+        posX = GetComponent<Rigidbody2D>().position.x;
+        posY = GetComponent<Rigidbody2D>().position.y;
+        startX = GetComponent<Rigidbody2D>().position.x;
+        startY = GetComponent<Rigidbody2D>().position.y;
+        falling = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (type.Equals("Track"))
+        {
+            if ((endX > 0 && ((posX < startX && velocityX < 0) || (posX > endX + startX && velocityX > 0)))
+               || (endX < 0 && ((posX > startX && velocityX > 0) || (posX < endX + startX && velocityX < 0))))
+            {
+                velocityX = -1 * velocityX;
+            }
+            if ((endY > 0 && ((posY < startY && velocityY < 0) || (posY > endY + startY && velocityY > 0)))
+               || (endY < 0 && ((posY > startY && velocityY > 0) || (posY < endY + startY && velocityY < 0))))
+            {
+                velocityY = -1 * velocityY;
+            }
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
+            posX = GetComponent<Rigidbody2D>().position.x;
+            posY = GetComponent<Rigidbody2D>().position.y;
+        }
+
+        if (type.Equals("Falling"))
+        {
+            if (GetComponentInChildren<TriggeredObject>().isTriggerSetOff())
+            {
+                GetComponent<Rigidbody2D>().gravityScale = 2;
+            }    
+        }
+
+        if (type.Equals("Loop"))
+        {
+            if ((endX > 0 && posX > endX + startX) || (endX < 0 && posX < endX + startX)
+               || (endY > 0 && posY > endY + startY) || (endY < 0 && posY < endY + startY))
+            {
+                GetComponent<Transform>().position = new Vector3(startX, startY, 0);
+            }
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
+            posX = GetComponent<Rigidbody2D>().position.x;
+            posY = GetComponent<Rigidbody2D>().position.y;
+        }
+
+
+    }
+}
