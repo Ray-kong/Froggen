a1e7ce5e1a223556d2d90d90f66dde7ee9359cb2
djrosenthal27 <djrosenthal004@gmail.com>
2023-03-28 16:06:31 -0400

    Misc. Changes
    Changes made with Ray during class time, including bug fixes such as sliding walls and turning off player colliders when they die. Also replaced all boxes with the box prefab

--- Assets/Script/PlayerLife.cs
+++ Assets/Script/PlayerLife.cs
@@ -7,36 +7,42 @@ public class PlayerLife : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+    private BoxCollider2D co;
     // Start is called before the first frame update
     private void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-
+        co = GetComponent<BoxCollider2D>();
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("death")) {
+        if (collision.gameObject.CompareTag("death"))
+        {
             Die();
         }
     }
+
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.CompareTag("death"))
         {
-            Die();
+              Die();
         }
         if (collider.gameObject.CompareTag("trigger"))
         {
             collider.gameObject.GetComponent<TriggeredObject>().TriggerSetOff();
+           
         }
     }
 
-    private void Die()
+    public void Die()
     {
         anim.SetTrigger("death");
+        co.enabled = false;
         rb.bodyType = RigidbodyType2D.Static;
     }
 
