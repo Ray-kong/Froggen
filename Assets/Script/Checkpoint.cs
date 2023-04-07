04d65ba67a599f90292e373e7f93caa31e74a194
FenixSC2 <nathanbarth3141@gmail.com>
2023-04-07 15:14:59 -0400

    Updates
    Added flys
    Added animated checkpoints
    Added additional spikes at beginning

--- Assets/Script/Checkpoint.cs
+++ Assets/Script/Checkpoint.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(collision.gameObject.name);
+        anim.SetBool("ifChecked", true);
+
+
     }
 }
