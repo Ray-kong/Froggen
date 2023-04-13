e84117c5dfa709bae5cecda7dc0e02287cc14900
RayKong <ray.hyun.kong@gmail.com>
2023-04-13 14:44:34 -0400

    finished mountain movement
    i hate my life kinda

--- Assets/Script/MountainMover.cs
+++ Assets/Script/MountainMover.cs
@@ -5,28 +5,12 @@ using UnityEngine;
 public class MountainMover : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    //private Camera camera = GetComponent<Camera>();
-
-    // Update is called once per frame
 
 
-    private void Start()
-    {
-        OrthographicCamera.enabled = true;
-
-    }
     private void Update()
     {
-        if (Input.GetKey(KeyCode.G))
-        {
-            OrthographicCamera.orthographicSize = 15;
-            transform.position = new Vector3(player.position.x, 0f, transform.position.z);
+            transform.position = new Vector3(player.position.x, 5.5f, transform.position.z);
             Debug.Log("this worked");
-        }
-        else
-        {
-            OrthographicCamera.orthographicSize = 6;
-            transform.position = new Vector3(player.position.x, 0f, transform.position.z);
-        }
+
     }
 }
