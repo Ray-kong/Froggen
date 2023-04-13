5075cdc33b3888f5b26354d58d0a634f33de3b3b
FenixSC2 <nathanbarth3141@gmail.com>
2023-04-13 13:59:36 -0400

    Graphics
    Added graphics, made minor gameplay updates

---
+++ Assets/Script/MountainMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountainMover : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    //private Camera camera = GetComponent<Camera>();
+
+    // Update is called once per frame
+
+
+    private void Start()
+    {
+        OrthographicCamera.enabled = true;
+
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.G))
+        {
+            OrthographicCamera.orthographicSize = 15;
+            transform.position = new Vector3(player.position.x, 0f, transform.position.z);
+            Debug.Log("this worked");
+        }
+        else
+        {
+            OrthographicCamera.orthographicSize = 6;
+            transform.position = new Vector3(player.position.x, 0f, transform.position.z);
+        }
+    }
+}
